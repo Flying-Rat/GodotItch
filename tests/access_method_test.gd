e7ab9faeeeb7f55c327cc8806a65bde737f49979
Jakub Hubacek <hubacek.jakub@gmail.com>
2025-09-10 14:34:59 +0200

    Remove deprecated tests

--- tests/access_method_test.gd
+++
@@ -1,7 +0,0 @@
-# Comprehensive Access Method and Basic Functionality Test
-# Tests different ways to access the plugin and validates core functionality
-extends Node
-
-func _ready():
-// Moved to tests/old/access_method_test.gd
-// This file kept as a placeholder to avoid path breakage.
